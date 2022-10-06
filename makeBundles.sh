8bfb14153acfe53411745b51537b091df4b0846e
kicha <ivankika.1999@gmail.com>
2022-10-06 16:33:21 +0300

    temporary buildable version

---
+++ makeBundles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ant concat allFlavors minify samples
+mkdir bundles
+cp dist/sv-c.js bundles/sv-c.js
+cp dist/m-c.js bundles/m-c.js
+cp dist/sv-c.min.js bundles/sv-c.min.js
+cp dist/m-c.min.js bundles/m-c.min.js
+echo 'BUILD COMPLETE!'
